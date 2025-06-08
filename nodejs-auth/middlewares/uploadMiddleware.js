db76690e514619df36b0da672792f5324be3ac88
Gaurav Kumar <gaurav2k9ssk4754@gmail.com>
2025-06-09 00:36:24 +0530

    added uplode folder safety

--- nodejs-auth/middlewares/uploadMiddleware.js
+++ nodejs-auth/middlewares/uploadMiddleware.js
@@ -1,30 +1,34 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs")
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    console.log(" __dirname + '/uploads'", path.join(process.cwd(), 'uploads'))
-    cb(null,path.join(process.cwd(), 'uploads'))
+    const uploadPath = path.join(process.cwd(), "uploads");
+    if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath);
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
-    console.log("file.fieldname + '-' + Date.now()+ path.extname(file.originalname)",file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
-    cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname));
-  }
-})
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
-const validateFileType = (req,file,cb) =>{
-    if( file.mimetype.startsWith("image")){
-        cb(null,true)
-    } else {
-        // cb(null, false)
-        cb(new Error('Not an Image!, Please upload only image.'))
-    }
-}
+const validateFileType = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    // cb(null, false)
+    cb(new Error("Not an Image!, Please upload only image."));
+  }
+};
 
 module.exports = multer({
   storage: storage,
   fileFilter: validateFileType,
   limits: {
-    fieldNameSize:  5 * 1024 * 1024,
+    fieldNameSize: 5 * 1024 * 1024,
   },
-})
+});
