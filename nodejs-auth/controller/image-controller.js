0e85057306830547e8b3b28ad72d6068c6bf5f68
Gaurav Kumar <gaurav2k9ssk4754@gmail.com>
2025-06-15 11:55:12 +0530

    added delete image

--- nodejs-auth/controller/image-controller.js
+++ nodejs-auth/controller/image-controller.js
@@ -1,56 +1,95 @@
-const cloudinary = require("../configs/cloudinaryConfig")
+const cloudinary = require("../configs/cloudinaryConfig");
 const { uploadImageToCloudinary } = require("../helpers/cloudinaryHelper");
-const Image = require('../model/image');
-const fs = require('node:fs')
+const Image = require("../model/image");
+const fs = require("node:fs");
+const Users = require("../model/user");
 
-const uploadImage = async (req,res) =>{
-    try {
-        if(!req.file){
-            return res.status(400).json({
-                success: false,
-                message: "Image file is required"
-            })
-        }
-        const { url, publicId } = await uploadImageToCloudinary(req.file.path);
-        const uploadedImage = await Image.create({
-            url,
-            publicId,
-            uploadedBy: req.user.userId,
-        })
-        fs.unlinkSync(req.file.path);
-        return res.status(201).json({
-            success: true,
-            message: "Successfully uploaded the image",
-            data : uploadedImage
-        })
-    } catch (error) {
-        return res.status(500).json({
+const uploadImage = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        message: "Image file is required",
+      });
+    }
+    const { url, publicId } = await uploadImageToCloudinary(req.file.path);
+    const uploadedImage = await Image.create({
+      url,
+      publicId,
+      uploadedBy: req.user.userId,
+    });
+    fs.unlinkSync(req.file.path);
+    return res.status(201).json({
+      success: true,
+      message: "Successfully uploaded the image",
+      data: uploadedImage,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const getAllImageController = async (req, res) => {
+  try {
+    const images = await Image.find({});
+    if (images) {
+      return res.status(500).json({
+        success: true,
+        message: "Successfully fetched all images",
+        data: images,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+      message: "Network error while fetching data!",
+    });
+  }
+};
+
+const deleteImageController = async (req, res) => {
+  try {
+    const imageToDeleteId = req.params.imageId;
+    const authenticatedUser = req.user;
+
+    const imageFound = await Image.findById(imageToDeleteId);
+
+    if(!imageFound){
+        return res.status(400).json({
             success: false,
-            message: error.message
+            message: "No such image found"
         })
     }
-}
 
-const getAllImageController = async (req,res)=>{
-    try {
-       const images = await Image.find({});
-       if(images){
-        res.status(500).json({
-            success: true,
-            message: "Successfully fetched all images",
-            data: images,
-        })
-       }
-    } catch (error) {
-        res.status(500).json({
+    if(imageFound.uploadedBy.toString() !== authenticatedUser.userId){
+        return res.status(400).json({
             success: false,
-            error: error.message,
-            message: "Network error while fetching data!"
+            message: "Ca not delete the image"
         })
     }
-}
+    await cloudinary.uploader.destroy(imageFound.publicId);
+
+    await Image.findByIdAndDelete(imageToDeleteId);
+
+    return res.status(200).json({
+        success: true,
+        message: "Image deleted successfully!"
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+      message: "Network error while deleting image",
+    });
+  }
+};
 
 module.exports = {
-    uploadImage,
-    getAllImageController
-}
+  uploadImage,
+  getAllImageController,
+  deleteImageController,
+};
--- nodejs-auth/routes/image-routes.js
+++ nodejs-auth/routes/image-routes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleWare')
 const adminMiddleware = require("../middlewares/adminMiddleware")
-const {uploadImage, getAllImageController} = require("../controller/image-controller")
+const {uploadImage, getAllImageController,deleteImageController} = require("../controller/image-controller")
 const uploadMiddleware = require("../middlewares/uploadMiddleware")
 
 // "image" => key where image is being uploaded
 router.post("/upload",authMiddleware,adminMiddleware,uploadMiddleware.single('image'),uploadImage);
 
-router.get("/get",authMiddleware,getAllImageController)
+router.get("/get",authMiddleware,getAllImageController);
+router.delete("/delete/:imageId",authMiddleware,deleteImageController);
 
 module.exports = router;
